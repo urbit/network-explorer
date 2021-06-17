be1966257d410dd7fb847eeb260b8732ba6b76d0
pkova <pyry.kovanen@gmail.com>
2021-06-17 23:36:32 +0300

    Refactor to store more state in node db

--- src/network_explorer/main.clj
+++ src/network_explorer/main.clj
@@ -50,24 +50,16 @@
      (.setTimeZone (java.util.TimeZone/getTimeZone "UTC")))
    s))
 
-(def ownership-query
-  '[:find (pull ?e [:pki-event/address])
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-    [?e :pki-event/node ?p]
-    [?e :pki-event/type :change-ownership]
-    [(q '[:find (max ?t)
-          :in $ ?urbit-id
-          :where
-          [?p :node/urbit-id ?urbit-id]
-          [?e :pki-event/node ?p]
-          [?e :pki-event/type :change-ownership]
-          [?e :pki-event/time ?t]] $ ?urbit-id) [[?newest]]]
-    [?e :pki-event/time ?newest]])
+(defn inc-attr
+  "Transaction function that increments the value of entity's card-1
+attr by amount, treating a missing value as 1."
+  [db entity attr amount]
+  (let [m (d/pull db {:eid entity :selector [:db/id attr]})]
+    [[:db/add (:db/id m) attr (+ (or (attr m) 1) amount)]]))
 
 (def management-proxy-query
   '[:find (pull ?e [:pki-event/address])
-    :in $ ?urbit-id
+    :in $ [?urbit-id ...]
     :where [?p :node/urbit-id ?urbit-id]
            [?e :pki-event/node ?p]
            [?e :pki-event/type :change-management-proxy]
@@ -80,102 +72,23 @@
                  [?e :pki-event/time ?t]] $ ?urbit-id) [[?newest]]]
            [?e :pki-event/time ?newest]])
 
-(def voting-proxy-query
-  '[:find (pull ?e [:pki-event/address])
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-           [?e :pki-event/node ?p]
-           [?e :pki-event/type :change-voting-proxy]
-           [(q '[:find (max ?t)
-                 :in $ ?urbit-id
-                 :where
-                 [?p :node/urbit-id ?urbit-id]
-                 [?e :pki-event/node ?p]
-                 [?e :pki-event/type :change-voting-proxy]
-                 [?e :pki-event/time ?t]] $ ?urbit-id) [[?newest]]]
-           [?e :pki-event/time ?newest]])
-
-(def transfer-proxy-query
-  '[:find (pull ?e [:pki-event/address])
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-           [?e :pki-event/node ?p]
-           [?e :pki-event/type :change-transfer-proxy]
-           [(q '[:find (max ?t)
-                 :in $ ?urbit-id
-                 :where
-                 [?p :node/urbit-id ?urbit-id]
-                 [?e :pki-event/node ?p]
-                 [?e :pki-event/type :change-transfer-proxy]
-                 [?e :pki-event/time ?t]] $ ?urbit-id) [[?newest]]]
-           [?e :pki-event/time ?newest]])
-
-(def spawn-proxy-query
-  '[:find (pull ?e [:pki-event/address]) ?newest
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-           [?e :pki-event/node ?p]
-           [?e :pki-event/type :change-spawn-proxy]
-           [(q '[:find (max ?t)
-                 :in $ ?urbit-id
-                 :where
-                 [?p :node/urbit-id ?urbit-id]
-                 [?e :pki-event/node ?p]
-                 [?e :pki-event/type :change-spawn-proxy]
-                 [?e :pki-event/time ?t]] $ ?urbit-id) [[?newest]]]
-           [?e :pki-event/time ?newest]])
-
-(def sponsor-query
-  '[:find (pull ?e [:pki-event/type]) (pull ?s [:node/urbit-id])
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-           [?e :pki-event/node ?p]
-           (or [?e :pki-event/type :lost-sponsor]
-               [?e :pki-event/type :escaped])
-           [?e :pki-event/target-node ?s]
-           [(q '[:find (max ?t)
-                 :in $ ?urbit-id
-                 :where
-                 [?p :node/urbit-id ?urbit-id]
-                 [?e :pki-event/node ?p]
-                 (or [?e :pki-event/type :lost-sponsor]
-                     [?e :pki-event/type :escaped])
-                 [?e :pki-event/time ?t]] $ ?urbit-id) [[?newest]]]
-           [?e :pki-event/time ?newest]])
-
 (def owners-query
   '[:find (count ?e)
-    :in $ ?urbit-id
+    :in $ [?urbit-id ...]
     :where [?p :node/urbit-id ?urbit-id]
            [?e :pki-event/node ?p]
            [?e :pki-event/type :change-ownership]])
 
-(def revision-query
-  '[:find (max ?r)
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-           [?e :pki-event/node ?p]
-           [?e :pki-event/type :change-networking-keys]
-           [?e :pki-event/revision ?r]])
-
-(def continuity-query
-  '[:find (max ?c)
-    :in $ ?urbit-id
-    :where [?p :node/urbit-id ?urbit-id]
-           [?e :pki-event/node ?p]
-           [?e :pki-event/type :broke-continuity]
-           [?e :pki-event/continuity ?c]])
-
 (def spawned-query
   '[:find (pull ?e [*])
-    :in $ ?urbit-id
+    :in $ [?urbit-id ...]
     :where [?p :node/urbit-id ?urbit-id]
            [?e :pki-event/target-node ?p]
            [?e :pki-event/type :spawn]])
 
 (def activated-query
   '[:find (pull ?e [*])
-    :in $ ?urbit-id
+    :in $ [?urbit-id ...]
     :where [?p :node/urbit-id ?urbit-id]
            [?e :pki-event/node ?p]
            [?e :pki-event/type :activate]])
@@ -183,10 +96,17 @@
 (def urbit-id-query
   '[:find (pull ?e [:node/urbit-id
                     :node/type
+                    [:node/revision :default 0]
+                    [:node/continuity :default 0]
+                    [:node/ownership-address :default nil]
+                    [:node/management-proxy :default nil]
+                    [:node/transfer-proxy :default nil]
+                    [:node/voting-proxy :default nil]
+                    [:node/spawn-proxy :default nil]
                     [:node/online :default false]
                     {:node/sponsor [:node/urbit-id]}
                     {[:node/_sponsor :as :node/kids :default []] [:node/urbit-id]}])
-    :in $ ?urbit-id
+    :in $ [?urbit-id ...]
     :where [?e :node/urbit-id ?urbit-id]])
 
 (defn set-lookup-ref [nodes-in-db urbit-id]
@@ -206,45 +126,57 @@
            {:node/sponsor {:db/id [:node/urbit-id (ob/sein s)]}})))
 
 (defn pki-line->txs [idx l]
-  (let [pki [(merge {:pki-event/id   idx
-                     :pki-event/time (parse-pki-time (first l))
-                     :pki-event/node {:db/id [:node/urbit-id (second l)]}}
-                    (case (nth l 2)
-                      "keys"         {:pki-event/type :change-networking-keys
-                                      :pki-event/revision (Long/valueOf (nth l 3))}
-                      "breached"     {:pki-event/type :broke-continuity
-                                      :pki-event/continuity (Long/valueOf (nth l 3))}
-                      "management-p" {:pki-event/type :change-management-proxy
-                                      :pki-event/address (nth l 3)}
-                      "transfer-p"   {:pki-event/type :change-transfer-proxy
-                                      :pki-event/address (nth l 3)}
-                      "spawn-p"      {:pki-event/type :change-spawn-proxy
-                                      :pki-event/address (nth l 3)}
-                      "voting-p"     {:pki-event/type :change-voting-proxy
-                                      :pki-event/address (nth l 3)}
-                      "owner"        {:pki-event/type :change-ownership
-                                      :pki-event/address (nth l 3)}
-                      "activated"    {:pki-event/type :activate}
-                      "spawned"      {:pki-event/type :spawn
-                                      :pki-event/target-node {:db/id [:node/urbit-id (nth l 3)]}}
-                      "invite"       {:pki-event/type :invite
-                                      :pki-event/target-node {:db/id [:node/urbit-id (nth l 3)]}
-                                      :pki-event/address (nth l 4)}
-                      "sponsor"      (case (nth l 3)
-                                       "escaped to"    {:pki-event/type :escaped
-                                                        :pki-event/target-node {:db/id [:node/urbit-id (nth l 4)]}}
-                                       "detached from" {:pki-event/type :lost-sponsor
-                                                        :pki-event/target-node {:db/id [:node/urbit-id (nth l 4)]}})
-                      "escape-req"   (case (nth l 3)
-                                       "canceled"   {:pki-event/type :escape-canceled}
-                                       {:pki-event/type :escape-requested
-                                        :pki-event/target-node {:db/id [:node/urbit-id (nth l 3)]}})))]]
-    (case (nth l 2)
-      "sponsor"
-      (case (nth l 3)
-        "escaped to" (conj pki [:db/add [:node/urbit-id (second l)] :node/sponsor [:node/urbit-id (nth l 4)]])
-        "detached from" (conj pki [:db/retract [:node/urbit-id (second l)] :node/sponsor [:node/urbit-id (nth l 4)]]))
-      pki)))
+  (let [base {:pki-event/id   idx
+              :pki-event/time (parse-pki-time (first l))
+              :pki-event/node {:db/id [:node/urbit-id (second l)]}}]
+    [(case (nth l 2)
+       "keys"         [(merge base {:pki-event/type :change-networking-keys
+                                    :pki-event/revision (Long/valueOf (nth l 3))})
+                       {:node/urbit-id (second l)
+                        :node/revision (Long/valueOf (nth l 3))}]
+       "breached"     [(merge base {:pki-event/type :broke-continuity
+                                    :pki-event/continuity (Long/valueOf (nth l 3))})
+                       {:node/urbit-id (second l)
+                        :node/continuity (Long/valueOf (nth l 3))}]
+       "management-p" [(merge base {:pki-event/type :change-management-proxy
+                                    :pki-event/address (nth l 3)})
+                       {:node/urbit-id (second l)
+                        :node/management-proxy (nth l 3)}]
+       "transfer-p"   [(merge base {:pki-event/type :change-transfer-proxy
+                                    :pki-event/address (nth l 3)})
+                       {:node/urbit-id (second l)
+                        :node/transfer-proxy (nth l 3)}]
+       "spawn-p"      [(merge base {:pki-event/type :change-spawn-proxy
+                                    :pki-event/address (nth l 3)})
+                       {:node/urbit-id (second l)
+                        :node/spawn-proxy (nth l 3)}]
+       "voting-p"     [(merge base {:pki-event/type :change-voting-proxy
+                                    :pki-event/address (nth l 3)})
+                       {:node/urbit-id (second l)
+                        :node/voting-proxy (nth l 3)}]
+       "owner"        [(merge base {:pki-event/type :change-ownership
+                                    :pki-event/address (nth l 3)})
+                       {:node/urbit-id (second l)
+                        :node/ownership-address (nth l 3)}
+                       `(network-explorer.main/inc-attr ~(second l) :node/num-owners 1)]
+       "activated"    [(merge base {:pki-event/type :activate})]
+       "spawned"      [(merge base {:pki-event/type :spawn
+                                    :pki-event/target-node {:db/id [:node/urbit-id (nth l 3)]}})]
+       "invite"       [(merge base {:pki-event/type :invite
+                                    :pki-event/target-node {:db/id [:node/urbit-id (nth l 3)]}
+                                    :pki-event/address (nth l 4)})]
+       "sponsor"      (case (nth l 3)
+                        "escaped to"    [(merge base {:pki-event/type :escaped
+                                                      :pki-event/target-node {:db/id [:node/urbit-id (nth l 4)]}})
+                                         {:node/urbit-id (second l)
+                                          :node/sponsor {:db/id [:node/urbit-id (nth l 4)]}}]
+                        "detached from" [(merge base {:pki-event/type :lost-sponsor
+                                                      :pki-event/target-node {:db/id [:node/urbit-id (nth l 4)]}})
+                                         [:db/retract [:node/urbit-id (second l)] :node/sponsor [:node/urbit-id (nth l 4)]]])
+       "escape-req"   (case (nth l 3)
+                        "canceled"   [(merge base {:pki-event/type :escape-canceled})]
+                        [(merge base {:pki-event/type :escape-requested
+                                      :pki-event/target-node {:db/id [:node/urbit-id (nth l 3)]}})]))]))
 
 (defn mapcat-indexed [f & colls]
  (apply concat (apply map-indexed f colls)))
@@ -260,16 +192,9 @@
 
 (def get-client (memoize (fn [] (d/client cfg))))
 
-(defn get-node [urbit-id db]
-  (merge (ffirst (d/q urbit-id-query db urbit-id))
-         {:node/continuity (ffirst (d/q continuity-query db urbit-id))}
-         {:node/revision (ffirst (d/q revision-query db urbit-id))}
-         {:node/num-owners (ffirst (d/q owners-query db urbit-id))}
-         {:node/ownership-address (ffirst (d/q ownership-query db urbit-id))}
-         {:node/management-proxy (ffirst (d/q management-proxy-query db urbit-id))}
-         {:node/spawn-proxy (ffirst (d/q spawn-proxy-query db urbit-id))}
-         {:node/voting-proxy (ffirst (d/q voting-proxy-query db urbit-id))}
-         {:node/transfer-proxy (ffirst (d/q transfer-proxy-query db urbit-id))}))
+(defn get-nodes [urbit-ids db]
+  (merge (first (d/q urbit-id-query db urbit-ids))
+         {:node/num-owners (first (d/q owners-query db urbit-ids))}))
 
 (defn get-node* [{:keys [datomic.ion.edn.api-gateway/data]}]
   (let [client (get-client)
@@ -282,7 +207,7 @@
        :body (json/write-str {:error "Invalid urbit-id"})}
       {:status 200
        :headers {"Content-Type" "application/json"}
-       :body (json/write-str (get-node urbit-id db))})))
+       :body (json/write-str (get-nodes [urbit-id] db))})))
 
 (def get-node-lambda-proxy
   (apigw/ionize get-node*))

4ccd4048741dada75ca33c0faea153866bc0832a
pkova <pyry.kovanen@gmail.com>
2021-06-24 13:52:52 +0300

    Transact using map instead of doseq giving lambda something to print

--- src/network_explorer/main.clj
+++ src/network_explorer/main.clj
@@ -276,8 +276,7 @@ attr by amount, treating a missing value as 1."
                            lines)]
     (d/transact conn {:tx-data no-sponsor})
     (d/transact conn {:tx-data node-txs})
-    (doseq [txs pki-txs]
-      (d/transact conn {:tx-data txs}))))
+    (map (fn [txs] (d/transact conn {:tx-data txs})) pki-txs)))
 
 (defn get-all-nodes [limit offset types db]
   (let [query (if (empty? types)

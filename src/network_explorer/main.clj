7728f8322a284b7bed30b6a3c2f96fd1d591e79c
pkova <pyry.kovanen@gmail.com>
2021-06-24 14:17:56 +0300

    Fix off by one error in update-pki-txs

--- src/network_explorer/main.clj
+++ src/network_explorer/main.clj
@@ -272,7 +272,7 @@ attr by amount, treating a missing value as 1."
         no-sponsor (map node->node-tx-no-sponsor nodes)
         node-txs   (map node->node-tx nodes)
         pki-txs    (mapcat pki-line->txs
-                           (range newest-id (+ (inc newest-id) (count lines)))
+                           (range (inc newest-id) (+ (inc newest-id) (count lines)))
                            lines)]
     (d/transact conn {:tx-data no-sponsor})
     (d/transact conn {:tx-data node-txs})

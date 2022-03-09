28ba9753ff4b92323a0e714e3f91f697f8ae5bdf
pkova <pyry.kovanen@gmail.com>
2022-03-09 20:20:05 +0200

    Update s3-sync.sh to latest deployed version

--- gaze/s3-sync.sh
+++ gaze/s3-sync.sh
@@ -1,1 +1,1 @@
-curl --cookie "urbauth-~zod=0v5.hnjjv.rgc2o.f2l9r.tqe1h.k7c60" http://localhost:8080/~/scry/gaze/raw.txt | aws s3 cp - s3://gaze-exports/events.txt --acl public-read --content-type 'text/plain'
+curl --cookie "urbauth-~zod=0v5.hv8om.uc1m6.iditk.phm5g.vmi19" http://localhost:80/~/scry/gaze/raw.txt | aws s3 cp - s3://gaze-exports/events.txt --acl public-read --content-type 'text/plain'

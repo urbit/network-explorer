47d7fd76d9e0a1707596e886e1729d0daf1285ee
pkova <pyry.kovanen@gmail.com>
2022-04-12 21:48:27 +0300

    Update data sources for ongoing L2 data pipeline

--- gaze/s3-sync.sh
+++ gaze/s3-sync.sh
@@ -1,1 +1,1 @@
-curl --cookie "urbauth-~zod=0v3.bip2n.r0k5v.1hd63.5av0t.77jh9" http://localhost:8080/~/scry/gaze/raw.txt | aws s3 cp - s3://gaze-exports/events.txt --acl public-read --content-type 'text/plain'
+curl --cookie "urbauth-~zod=0v3.bip2n.r0k5v.1hd63.5av0t.77jh9" http://localhost:8080/~/scry/gaze/raw.txt | aws s3 cp - s3://gaze-exports/events4.txt --acl public-read --content-type 'text/plain'

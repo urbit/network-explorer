27ed6657bad0365903cedadd30912e10d6702e1c
pkova <pyry@urbit.org>
2023-03-20 21:54:01 +0200

    Fix stackoffset in UI

--- ui/src/OnlineShipsChart.jsx
+++ ui/src/OnlineShipsChart.jsx
@@ -15,6 +15,7 @@ export function OnlineShipsChart(props) {
   return(
     <ResponsiveContainer height='100%'>
       <BarChart
+        stackOffset='sign'
         data={onlineShips}>
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis
@@ -30,7 +31,7 @@ export function OnlineShipsChart(props) {
         />
         <YAxis type='number' orientation='right' tick={{fontSize: 10}} />
         <Tooltip />
-        <Bar dot={false} name='Churned' dataKey='churned' stroke='#BF421B' fill='#BF421B' stackId='b' />
+        <Bar dot={false} name='Churned' dataKey='churned' stroke='#BF421B' fill='#BF421B' stackId='a' />
         <Bar dot={false} name='Retained' dataKey='retained' stroke='#219DFF' fill='#219DFF' stackId='a' />
         <Bar dot={false} name='New' dataKey='new' stroke='#00B171' fill='#00B171' stackId='a' />
         <Bar dot={false} name='Resurrected' dataKey='resurrected' stroke='#DD9C34' fill='#DD9C34' stackId='a'  />

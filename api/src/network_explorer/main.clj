5794d4d443cd17912ed3ac460e99243f87f94b0b
pkova <pyry@urbit.org>
2023-05-01 14:09:11 +0300

    Update historic

--- api/src/network_explorer/main.clj
+++ api/src/network_explorer/main.clj
@@ -1032,7 +1032,7 @@ attr by amount, treating a missing value as 1."
   (pr-str (count (update-aggregate-status conn db))))
 
 (defn update-radar-data [_]
-  (let [historic 1181993
+  (let [historic 1205712
         client (get-client)
         conn   (d/connect client {:db-name "network-explorer-2"})
         db     (d/db conn)
